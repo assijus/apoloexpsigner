4bcce59f9582cd914c93b95cf3cee7c9b13d767e
Renato Crivano <renato.crivano@gmail.com>
2017-10-31 18:10:45 -0200

    Corrigindo o REDIS

--- src/main/java/br/jus/trf2/apoloexp/signer/MemCacheRedis.java
+++ src/main/java/br/jus/trf2/apoloexp/signer/MemCacheRedis.java
@@ -36,27 +36,27 @@ public class MemCacheRedis implements IMemCache {
 	}
 
 	public static int getDatabase() {
-		return Integer.parseInt(Utils.getProperty("redis.database", "10"));
+		return Integer.parseInt(SwaggerUtils.getProperty("assijus.redis.database", "10"));
 	}
 
 	public static String getPassword() {
-		return Utils.getProperty("redis.password", null);
+		return SwaggerUtils.getProperty("assijus.redis.password", null);
 	}
 
 	public static int getSlavePort() {
-		return Integer.parseInt(Utils.getProperty("redis.slave.port", "0"));
+		return Integer.parseInt(SwaggerUtils.getProperty("assijus.redis.slave.port", "0"));
 	}
 
 	public static String getSlaveHost() {
-		return Utils.getProperty("redis.slave.host", null);
+		return SwaggerUtils.getProperty("assijus.redis.slave.host", null);
 	}
 
 	public static String getMasterHost() {
-		return Utils.getProperty("redis.master.host", "localhost");
+		return SwaggerUtils.getProperty("assijus.redis.master.host", "localhost");
 	}
 
 	public static int getMasterPort() {
-		return Integer.parseInt(Utils.getProperty("redis.master.port", "6379"));
+		return Integer.parseInt(SwaggerUtils.getProperty("assijus.redis.master.port", "6379"));
 	}
 
 	@Override
